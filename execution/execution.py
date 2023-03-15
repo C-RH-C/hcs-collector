e59732f98deccbc2f99342adc35fe841f0574b2b
starnold <starnold@redhat.com>
2023-03-15 18:13:52 +0000

    dowbload tags

--- execution/execution.py
+++ execution/execution.py
@@ -93,8 +93,8 @@ def append_tags_to_inventory_csv(dest_csv_file, crhc_cli):
                             tag_string = tag_string + tag.get("key") + "=" + tag.get("value")
                             print(tag_string)
                         row.append(tag_string)
-    with open(dest_csv_file, "w") as file_obj:
-        mywriter = csv.writer(file_obj,delimiter=',') # ,quotechar='"'
+    with open(dest_csv_file, "w") as file_csv:
+        mywriter = csv.writer(file_csv,delimiter=',') # ,quotechar='"'
         mywriter.writerows(csv_file)
 
 def append_tags_to_inventory_json(dest_json_file, crhc_cli):
