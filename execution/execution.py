bdc2ad321a0a203540ba62f84bc8ddc70e9a6b71
starnold <starnold@redhat.com>
2023-03-15 17:43:37 +0000

    dowbload tags

--- execution/execution.py
+++ execution/execution.py
@@ -73,6 +73,8 @@ def append_tags_to_inventory_csv(dest_csv_file, crhc_cli):
         csv_file = csv.reader(file_obj)
         for row in csv_file:
             id = row[0];
+            print(row)
+            print("finding id : " + id)
             os.system(crhc_cli + " get /api/inventory/v1/hosts/" + id + "/tags > /tmp/tags.json")
             with open("/tmp/tags.json", "r") as file_tag:
                 tag_result=json.load(file_tag)
