63e0330afaadaf4c5cd06c114b08ebaf9b3ae0f9
Alex Matveyev <30264249+alexmatveyev@users.noreply.github.com>
2023-05-15 11:11:48 -0400

    [ENHANCEMENT] HA and RHDS (Directory Server) product cert check only
    * ha and directory server product cert check
    * README.md update

--- execution/process_rhel_addons.py
+++ execution/process_rhel_addons.py
@@ -1,6 +1,7 @@
-import json
+import csv
+from execution import util
 
-def ondemand_rhel_related_products_from_json(path_to_json_dir, json_files_list, tag):
+def ondemand_rhel_related_products(path_to_csv_dir, csv_files_list, tag):
     """
     TODO
     """
@@ -9,67 +10,53 @@ def ondemand_rhel_related_products_from_json(path_to_json_dir, json_files_list, 
     # print(path_to_csv_dir)
     # print(csv_files_list)
 
-    CURRENT_TIMEFRAME_YEAR = path_to_json_dir.split("/")[4]
-    CURRENT_TIMEFRAME_MONTH = path_to_json_dir.split("/")[5]
+    CURRENT_TIMEFRAME_YEAR = path_to_csv_dir.split("/")[4]
+    CURRENT_TIMEFRAME_MONTH = path_to_csv_dir.split("/")[5]
     CURRENT_TIMEFRAME = CURRENT_TIMEFRAME_YEAR + "-" + CURRENT_TIMEFRAME_MONTH
 
-    rhel_ha = 0
-    rhel_directoryserver = 0
+    for sheet in csv_files_list:
 
-    for jsonFile in json_files_list:
-        
-        stage_rhel_ha = 0
+        # adding rhel_ha physical and virtual
+        rhel_ha_physical = 0
+        stage_rhel_ha_physical = 0
+        rhel_ha_virtual =0
+        stage_rhel_ha_virtual = 0
+        rhel_directoryserver = 0
         stage_rhel_directoryserver = 0
-        
 
-        with open(path_to_json_dir + "/" + jsonFile, "r") as file_obj:
-            data = json.load(file_obj)
-            if ('results' in data):
-                for inventoryItem in data['results']:
-                    # print(row)
-                    # get the number of cores.
-                    system_profile = inventoryItem['system_profile']
-                    installed_products = []
-                    installed_packages = []
-                    installed_services = []
-                    if ('installed_products' in system_profile): installed_products = system_profile['installed_products']
-                    if ('installed_packages' in system_profile): installed_packages = system_profile['installed_packages']
-                    if ('installed_services' in system_profile): installed_services =  system_profile['installed_services']
-               
+        with open(path_to_csv_dir + "/" + sheet, "r") as file_obj:
+            csv_file = csv.reader(file_obj)
+            for row in csv_file:
+                # print(row)
 
-                    installed_product_list = []
-                    for product in installed_products:
-                        installed_product_list.append(product['id'])
-                    
-                    ha_packages_installed = False
-                    directory_server_packages_installed = False
-                    for package_name in installed_packages:
-                        if (package_name.startswith("rgmanager") or package_name.startswith("pcs-") or package_name.startswith("pacemaker")): ha_packages_installed = True
-                        if (package_name.startswith("389-ds")): directory_server_packages_installed = True
+                infrastructure_type = row[21]
+                installed_product = row[35]
 
-                    directory_server_services_running = False
-                    ha_services_running = False
-                    for service_name in installed_services:
-                        if ("pcsd" in service_name): ha_services_running = True
-                        
+                # HA is covered by products 83, 84 and 159
+                if ('83' in installed_product) or ('84' in installed_product) or ('159' in installed_product):
+                    if infrastructure_type == "physical":
+                        stage_rhel_ha_physical = stage_rhel_ha_physical + 1
+                    elif infrastructure_type == "virtual":
+                        stage_rhel_ha_virtual = stage_rhel_ha_virtual + 1
 
-                    # HA is covered by products 159, 83, and 84
-                    if (('83' in installed_product_list) or ('84' in installed_product_list) or ('159' in installed_product_list) or ha_packages_installed or ha_services_running):
-                        stage_rhel_ha = stage_rhel_ha + 1
-                    
-                    # Directory Server is covered by products 159, 83, and 84
-                    if (('200' in installed_product_list) or directory_server_packages_installed):
+                # Directory Server is covered by product 200
+                if ('200' in installed_product):
                         stage_rhel_directoryserver = stage_rhel_directoryserver + 1
-        
-        if stage_rhel_ha > rhel_ha:
-            rhel_ha = stage_rhel_ha
-            stage_rhel_ha = 0
 
-        if stage_rhel_directoryserver > rhel_directoryserver:
-            rhel_directoryserver = stage_rhel_directoryserver
-            stage_rhel_directoryserver = 0
+    if stage_rhel_ha_physical > rhel_ha_physical:
+        rhel_ha_physical = stage_rhel_ha_physical
+        stage_rhel_ha_physical = 0
+
+    if stage_rhel_ha_virtual > rhel_ha_virtual:
+        rhel_ha_virtual = stage_rhel_ha_virtual
+        stage_rhel_ha_virtual = 0
+
+    if stage_rhel_directoryserver > rhel_directoryserver:
+        rhel_directoryserver = stage_rhel_directoryserver
+        stage_rhel_directoryserver = 0
 
 
-    print("On-Demand, HA Node ...........................: {}".format(rhel_ha))
+    print("On-Demand, High Availability, Physical Node...: {}".format(rhel_ha_physical))
+    print("On-Demand, High Availability, Virtual Node....: {}".format(rhel_ha_virtual))
     print("On-Demand, Directory Server Node .............: {}".format(rhel_directoryserver))
     print("")
